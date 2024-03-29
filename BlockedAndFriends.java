e52a71c1c390386939192f35c13bb8fc90cce3ff
asong18 <145512500+asong18@users.noreply.github.com>
2024-03-28 21:07:22 -0400

    Update BlockedAndFriends.java

--- BlockedAndFriends.java
+++ BlockedAndFriends.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class BlockedAndFriends implements BlockedUsers {
     private ArrayList<User> friends;
     private ArrayList<User> blocked;
+    private ArrayList<User> mutualFriends;
     private String username;
 
     public User() {
@@ -69,5 +70,46 @@ public class BlockedAndFriends implements BlockedUsers {
             return false;
         }
         return false;
+    }
+
+    public void setMutualFriends(User one, User two) {
+        if (one.getFriends == null || two.getFriends == null) {
+            return null;
+        }
+        ArrayList<User> friendOneFriends = new ArrayList<>;
+        friendOneFriends = one.getFriends;
+        ArrayList<User> friendTwoFriends= new ArrayList<>;
+        friendTwoFriends = two.getFriends;
+        ArrayList<User> mutualFriends= new ArrayList<>;
+        for (int i = 0; i < one.getSize(), i++){
+            if (two.contains(one.get(i))){
+                mutualFriends.add(one.get(i));
+            }
+        }
+        this.mutualFriends = mutualFriends;
+    }
+    public void getMutualFriends(){
+        return this.mutualFriends;
+    }
+    public String mutualFriendsToString (User user) {
+        ArrayList<User> mutuals = new ArrayList<>;
+        if (mutuals.getSize()==0 || mutuals == null){
+            return null;
+        }
+        int numberMutuals = mutuals.getSize();
+        if (numberMutuals <= 3) {
+            if (numberMutuals == 1){
+                return String.format("Friends with: %s",mutuals.getFirst());
+            } else if (numberMutuals == 2){
+                return String.format("Friends with: %s, %s", mutuals.getFirst(), mutuals.getLast());
+            } else {
+                return String.format("Friends with %s, %s, %s",mutuals.getFirst(), mutuals.get(2), mutuals.getLast());
+            }
+        }
+        return String.format("Friends with: %s, %s, %s and %d others",mutuals.getFirst(),mutuals.get(2),mutuals.getLast(),numberMutuals-3);
+    } 
+    public ArrayList<User> recommendedFriends(User user){
+        ArrayList<User> inputFriends= new ArrayList<>;
+        
     }
 }
