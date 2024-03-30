6cd3f14eaa1a98e815778003e3d6aed80c77933d
Archit Malviya <165109450+architmlv@users.noreply.github.com>
2024-03-29 21:43:30 -0400

    User.java
    Removed friendlist, blocked list, added constructors

--- User.java
+++ User.java
@@ -1,35 +1,23 @@
 import java.util.ArrayList;
-
 public class User {
-
-    private String id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
     private String photoId;
-    private ArrayList<User> friendList;
-    private ArrayList<User> blockList;
+    private String username;
 
-    public User (String id, String firstName, String lastName, String email, String password, String photoId,
-                 ArrayList<String> friendList,ArrayList<String> blockList){
+    public User (String id, String firstName, String lastName, String email, String password, String photoId){
         this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
         this.photoId = photoId;
-        this.friendList = friendList;
-        this.blockList = blockList;
+    }
+    public User (){
     }
 
-
-    public String getId() {
-        return id;
-    }
-    public void setId(String id) {
-        this.id = id;
-    }
     public String getFirstName() {
         return firstName;
     }
@@ -55,36 +43,22 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
-    public String getUsername() {
-        return username;
-    }
-    public void setUsername(String username) {
-        this.username = username;
-    }
     public String getPhotoId() {
         return photoId;
     }
     public void setPhotoId(String photoId) {
         this.photoId = photoId;
     }
-
-    public ArrayList<User> getFriendList() {
-        return friendList;
+    public String getUsername() {
+        return username;
     }
-    public ArrayList<User> getBlockList() {
-        return blockList;
-    }
-    public void setFriendList(ArrayList<User> friendList) {
-        this.friendList = friendList;
-    }
-    public void setBlockList(ArrayList<User> blockList) {
-        this.blockList = blockList;
+    public void setUsername(String username) {
+        this.username = username;
     }
 
     @Override
     public String toString() {
-        return this.id + "," +
-                this.firstName + "," +
+        return this.firstName + "," +
                 this.lastName + "," +
                 this.email + "," +
                 this.username + "," +
