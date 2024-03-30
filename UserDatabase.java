6d97038c0d2a4351b1fc2cf627a1c87075b01b1f
Archit Malviya <165109450+architmlv@users.noreply.github.com>
2024-03-29 22:16:43 -0400

    UserDatabase.java

--- UserDatabase.java
+++ UserDatabase.java
@@ -1,31 +1,21 @@
 import java.io.*;
 import java.util.HashMap;
-import java.util.UUID;
 
 public class UserDatabase {
-    private HashMap<String,User> userMap = new HashMap<String,User>();
+    private static HashMap<String,User> userMap = new HashMap<String,User>();
     public UserDatabase(){
         readUsersFromFile();
-        //Message file Read
     }
 
 
-    public boolean saveUser(String id,User user) {
-        this.userMap.put(id,user);
-        FileReadWriter.writeFile("User.db", this.userMap);
+    public boolean saveUser(String username,User user) {
+        userMap.put(username,user);
+        FileReadWriter.writeFile("User.db", userMap);
         return true;
     }
 
-    public User getUserById(String id) {
 
-        if (this.userMap.containsKey(id))
-            return this.userMap.get(id);
-
-        return null;
-
-    }
-
-    public User getUserByUsername(String username) {
+    public static User getUserByUsername(String username) {
         for (User user : userMap.values()) {
             if (user.getUsername().equalsIgnoreCase(username)) {
                 return user;
@@ -48,7 +38,7 @@ public class UserDatabase {
         return userMap;
     }
     public void setUserMap(HashMap<String, User> userMap) {
-        this.userMap = userMap;
+        UserDatabase.userMap = userMap;
     }
     private void readUsersFromFile() {
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("User.db"))) {
@@ -59,13 +49,11 @@ public class UserDatabase {
                 if (tempArray.length > 6 )
                 {
                     User user = new User();
-                    user.setId(tempArray[0]);
-                    user.setFirstName(tempArray[1]);
-                    user.setLastName(tempArray[2]);
-                    user.setEmail(tempArray[3]);
-                    user.setUsername(tempArray[4]);
-                    user.setPassword(tempArray[5]);
-                    this.userMap.put(tempArray[0],user);
+                    user.setFirstName(tempArray[0]);
+                    user.setLastName(tempArray[1]);
+                    user.setUsername(tempArray[2]);
+                    user.setPassword(tempArray[3]);
+                    userMap.put(tempArray[0],user);
                 }
             }
         } catch (IOException e) {
