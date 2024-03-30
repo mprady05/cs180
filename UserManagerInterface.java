ab7211ef87c23c783a39bdf7045316e16c05b1b3
Archit Malviya <165109450+architmlv@users.noreply.github.com>
2024-03-30 18:51:34 -0400

    UserManagerInterface.java

---
+++ UserManagerInterface.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.Map;
+
+public interface UserManagerInterface {
+    User createUser(Map<String, String> userMap) throws SMPException;
+    String updateUser(String username, Map<String, String> userMap) throws SMPException;
+    String login(Map<String, String> userMap) throws SMPException;
+    boolean resetPassword(String username, String newPassword) throws SMPException;
+    boolean addFriend(User user, User friend);
+    boolean removeFriend(User friend);
+    boolean blockUser(User userBlocked);
+    boolean unblockUser(User userBlocked);
+    ArrayList<User> MutualFriends(User one, User two);
+    String mutualFriendsToString(ArrayList<User> mutuals);
+}
