e617805ee59c7381a3d41420577c17a6213056fd
Archit Malviya <165109450+architmlv@users.noreply.github.com>
2024-03-30 23:26:49 -0400

    UserDatabaseInterface.java

--- UserDatabaseInterface.java
+++ UserDatabaseInterface.java
@@ -2,7 +2,7 @@ import java.util.HashMap;
 
 public interface UserDatabaseInterface {
     boolean saveUser(String username, User user);
-    boolean deleteUser(String id);
+    boolean deleteUser(String username);
     HashMap<String, User> getUserMap();
     void setUserMap(HashMap<String, User> userMap);
 }
