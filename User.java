6dd25bc8960e09d091da7b9f19978e8e12f175c7
iyanaman <143299629+iyanaman@users.noreply.github.com>
2024-03-31 04:45:56 -0400

    Update User.java
    - updates the fields to add the postIds
    - updates user to shift to news feed functionality

--- User.java
+++ User.java
@@ -1,78 +1,168 @@
 import java.util.ArrayList;
-public class User implements UserInterface {
-    private String firstName;
-    private String lastName;
-    private String password;
-    private String photoId;
-    private String username;
-    private ArrayList<User> friendList;
-    private ArrayList<User> blockList;
+public class User {
+    private final String firstName;
+    private final String lastName;
+    private final String password;
+    private final String username;
+    private ArrayList<String> friendList;
+    private ArrayList<String> blockList;
+    private ArrayList<String> postIds;
 
-    public User (String firstName, String lastName, String password, String username, String photoId, ArrayList<User> friendList, ArrayList<User> blockList ){
+    /**
+     * Initializes a new User object with specified details.
+     * @param firstName User's first name.
+     * @param lastName User's last name.
+     * @param username User's username (must be unique).
+     * @param password User's password.
+     * @param friendList List of user's friends' usernames.
+     * @param blockList List of usernames blocked by the user.
+     * @param postIds List of post IDs created by the user.
+     */
+    public User (String firstName, String lastName, String username, String password, ArrayList<String> friendList, ArrayList<String> blockList, ArrayList<String> postIds) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.username = username;
         this.password = password;
-        this.photoId = photoId;
         this.friendList = friendList;
         this.blockList = blockList;
-        this.username = username;
-    }
-
-    public User(){  
+        this.postIds = postIds;
     }
 
     public String getFirstName() {
         return firstName;
     }
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
     public String getLastName() {
         return lastName;
-    }
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public String getPhotoId() {
-        return photoId;
-    }
-    public void setPhotoId(String photoId) {
-        this.photoId = photoId;
     }
     public String getUsername() {
         return username;
     }
-    public void setUsername(String username) {
-        this.username = username;
+    public String getPassword() {
+        return password;
     }
-
-    public ArrayList<User> getFriendList() {
+    public ArrayList<String> getFriendList() {
         return friendList;
     }
-
-    public ArrayList<User> getBlockList() {
+    public ArrayList<String> getBlockList() {
         return blockList;
     }
-
-    public void setFriendList(ArrayList<User> friendList) {
-        this.friendList = friendList;
+    public ArrayList<String> getPostIds() {
+        return postIds;
     }
 
-    public void setBlockList(ArrayList<User> blockList) {
-        this.blockList = blockList;
+    /**
+     * Adds a friend to the user's friend list and updates the user in the UsersManager.
+     * @param username Username of the new friend.
+     */
+    public void addFriend(String username) throws SMPException {
+        friendList.add(username);
+        UsersManager.updateUser(this);
     }
 
-    @Override
+    /**
+     * Blocks a user, removing them from the friend list if present and adding to the block list.
+     * Updates both users' data in the UsersManager accordingly.
+     * @param usernameToBlock Username of the user to be blocked.
+     */
+    public void blockUser(String usernameToBlock) throws SMPException {
+        if (friendList.contains(usernameToBlock)) {
+            friendList.remove(usernameToBlock);
+        }
+        if (!blockList.contains(usernameToBlock)) {
+            blockList.add(usernameToBlock);
+        }
+        User userToBlock = UsersManager.searchUser(usernameToBlock);
+        if (userToBlock != null && userToBlock.getFriendList().contains(this.username)) {
+            userToBlock.getFriendList().remove(this.username);
+            UsersManager.updateUser(userToBlock);
+        }
+        UsersManager.updateUser(this);
+    }
+
+    /**
+     * Removes a friend from the user's friend list and updates the user in the UsersManager.
+     * @param username Username of the friend to remove.
+     */
+    public void removeFriend(String username) throws SMPException {
+        friendList.remove(username);
+        UsersManager.updateUser(this);
+    }
+
+    /**
+     * Adds a new post with the given content created by the user.
+     * @param content Content of the new post.
+     */
+    public void addPost(String content) throws SMPException {
+        String postId = PostsManager.addPost(this.username, content, 0, 0, new ArrayList<>());
+        Post post = PostsManager.searchPost(postId);
+        if (post == null) {
+            throw new SMPException("Not able to add post.");
+        }
+        postIds.add(postId);
+        UsersManager.updateUser(this);
+        PostsManager.updatePost(post);
+    }
+
+    /**
+     * Hides a post from the user's view by removing it from their postIds list.
+     * @param postId ID of the post to hide.
+     */
+    public void hidePost(String postId) throws SMPException {
+        if (postIds.contains(postId)) {
+            postIds.remove(postId);
+            UsersManager.updateUser(this);
+        }
+    }
+
+    /**
+     * Retrieves a list of post IDs created by the user's friends.
+     * @return A list of post IDs.
+     */
+    public ArrayList<String> getFriendsPosts() {
+        ArrayList<String> friendsPosts = new ArrayList<>();
+        for (String friendUsername : friendList) {
+            User friend = UsersManager.searchUser(friendUsername);
+            if (friend != null) {
+                friendsPosts.addAll(friend.getPostIds());
+            }
+        }
+        return friendsPosts;
+    }
+
+    /**
+     * Provides a string representation of the User object.
+     * @return A string containing the user's data.
+     */
     public String toString() {
-        return this.firstName + "," +
-                this.lastName + "," +
-                this.username + "," +
-                this.password;
+        String result = "";
+        result += firstName + ';';
+        result += lastName + ';';
+        result += username + ';';
+        result += password + ';';
+        result += "(";
+        for (int i = 0; i < friendList.size(); i++) {
+            result += friendList.get(i);
+            if (i < friendList.size() - 1) {
+                result += ",";
+            }
+        }
+        result += ");";
+        result += "(";
+        for (int i = 0; i < blockList.size(); i++) {
+            result += blockList.get(i);
+            if (i < blockList.size() - 1) {
+                result += ",";
+            }
+        }
+        result += ");";
+        result += "(";
+        for (int i = 0; i < postIds.size(); i++) {
+            result += postIds.get(i);
+            if (i < postIds.size() - 1) {
+                result += ",";
+            }
+        }
+        result += ")";
+        return result;
     }
 }
