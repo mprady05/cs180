fefab308a66ce10ae56639e64ae4c0f8b13b8b0a
asong18 <145512500+asong18@users.noreply.github.com>
2024-03-31 10:16:07 -0400

    Update PostsManagerInterface.java

--- src/PostsManagerInterface.java
+++ src/PostsManagerInterface.java
@@ -2,4 +2,12 @@ import java.util.ArrayList;
 
 public interface PostsManagerInterface.java {
   ArrayList<Post> getPosts();
-  
+  void setPosts(ArraryList<Post> posts)
+  void readPostsDatabaseFile();
+  void writePostsDatabaseFile();
+  String addPost(String creatorUsername, String content, int upvotes, int downvotes, ArrayList,String> throws SMPException;
+  void clearAllPosts();
+  boolean updatePost(Post updatedPost) throws SMPException;
+  Post searchPost(String postId);
+  String getPostIdFromComment(Comment comment);
+}
