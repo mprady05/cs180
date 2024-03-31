d0ba9d78521b716bb06d4220bb34dc275af0bedd
asong18 <145512500+asong18@users.noreply.github.com>
2024-03-31 14:10:42 -0400

    Update CommentsManagerInterface.java

--- src/CommentsManagerInterface.java
+++ src/CommentsManagerInterface.java
@@ -1,1 +1,5 @@
-
+public interface CommentsManagerInterface {
+    void readCommentsDatabaseFile() throws SMPException;
+    void writeCommentsDatabaseFile() throws SMPException;
+    boolean updateComment(Comment updatedComment) throws SMPException;
+}
