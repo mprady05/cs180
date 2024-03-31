c48a5f5333df855ed44ed5522fa129a927c788ae
asong18 <145512500+asong18@users.noreply.github.com>
2024-03-31 10:44:16 -0400

    Update PostInterface.java

--- src/PostInterface.java
+++ src/PostInterface.java
@@ -1,1 +1,16 @@
+import java.util.ArrayList;
+import java.util.UUID;
 
+public interface PostInterface {
+  String getPostId();
+  User getCreator();
+  String getContent();
+  int getUpvotes();
+  int getDownvotes();
+  ArrayList<String> getComments();
+  void addUpvotes() throws SMPException;
+  void addDownvotes() throws SMPException;
+  void addComment(String author, String content) throws SMPException;
+  void deleteCommment(String commendId, String requesterUsername) throws SMPException;
+  String toString();
+}
