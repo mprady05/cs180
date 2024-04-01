9cfbaaa19a7871f26a07e14727e15329f2f3191e
iyanaman <143299629+iyanaman@users.noreply.github.com>
2024-03-31 20:54:25 -0400

    Create NewsFeedClientInterface.java
    Interface for phase 2 - NewsFeedClient

---
+++ src/NewsFeedClientInterface.java
@@ -0,0 +1,38 @@
+public interface NewsFeedClientInterface {
+    /**
+     * Displays the news feed to the user.
+     * This includes posts from friends, allowing the user to interact with them (e.g., upvote, downvote, comment).
+     */
+    void viewNewsFeed();
+
+    /**
+     * Allows the user to create a new post.
+     * @param content The content of the new post to be created.
+     */
+    void createPost(String content);
+
+    /**
+     * Allows the user to comment on a post.
+     * @param postId The unique ID of the post to be commented on.
+     * @param content The content of the comment to be added to the post.
+     */
+    void commentOnPost(String postId, String content);
+
+    /**
+     * Allows the user to upvote a post.
+     * @param postId The unique ID of the post to be upvoted.
+     */
+    void upvotePost(String postId);
+
+    /**
+     * Allows the user to downvote a post.
+     * @param postId The unique ID of the post to be downvoted.
+     */
+    void downvotePost(String postId);
+
+    /**
+     * Allows the user to hide a post from their news feed.
+     * @param postId The unique ID of the post to be hidden.
+     */
+    void hidePost(String postId);
+}
